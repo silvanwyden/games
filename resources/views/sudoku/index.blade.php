f2fa7eb54466940356959f39508b990100a44213
Silvan Wyden <silvan@wyden.com>
2018-09-20 20:13:01 +0200

    design changes

--- resources/views/sudoku/index.blade.php
+++ resources/views/sudoku/index.blade.php
@@ -59,11 +59,11 @@
                 @foreach ($temp as $temp2)
                     <div class="cell"><input style="
                         @if ($loop->iteration ==3 || $loop->iteration ==6)
-                            border-right-color: black;
+                            border-right-color: black; border-right-width: 2px;
                          @endif
 
                          @if ($loop->parent->iteration ==3 || $loop->parent->iteration ==6)
-                            border-bottom-color: black;
+                            border-bottom-color: black; border-bottom-width: 2px;
                         @endif
                              " name="cell[{{ $loop->parent->index }}][{{ $loop->index }}]" type="text" maxlength="1" value=
                         @if ($temp2 > 0)
